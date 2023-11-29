b87bcbdedbe0e9c72feffcd4f63d883ef42c28ba
NicoDestephen <ndestephen@gmail.com>
2023-11-29 12:04:41 +0100

    quest9

---
+++ src/DataFixtures/ProgramFixtures.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Program;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class ProgramFixtures extends Fixture implements DependentFixtureInterface
+{
+    const PROGRAMS = [
+        'Program1' => [
+                        'title'    => 'Walking dead',
+                        'synopsis' => 'Des zombies envahissent la terre',
+                        'category' => 'Action',
+                        'poster'   => 'lalala',],
+        'Program2' => [
+                        'title'    => 'Stranger Things',
+                        'synopsis' => 'Des enfants qui vont dans un monde parallèle',
+                        'category' => 'Horreur',
+                        'poster'   => 'lilili',],
+        'Program3' => [
+                        'title'    => 'The Witcher',
+                        'synopsis' => 'Un sorceleur qui tape des monstres',
+                        'category' => 'Horreur',
+                        'poster'   => 'lululu',],
+        'Program4' => [
+                        'title'    => 'Loki',
+                        'synopsis' => 'Les aventures de Loki à travers le temps',
+                        'category' => 'Fantastique',
+                        'poster'   => 'lololo',],
+        'Program5' => [
+                        'title'    => 'Bad Batch',
+                        'synopsis' => 'Les aventures d\'une équipe de clone après l\'ordre 66',
+                        'category' => 'Action',
+                        'poster'   => 'piou',],
+    ];
+    public function load(ObjectManager $manager)
+    {
+        foreach (self::PROGRAMS as $programName => $programDetails) {
+            $program = new Program();
+            $program->setTitle($programDetails['title']);
+            $program->setSynopsis($programDetails['synopsis']);
+            $program->setCategory($this->getReference('category_' . $programDetails['category']));
+            $program->setPoster($programDetails['poster']);
+            $manager->persist($program);
+        }
+
+        $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [
+          CategoryFixtures::class,
+        ];
+    }
+
+
+}
